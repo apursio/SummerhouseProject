1c03556d9fb8942fd5433a3c110b5a187b183d26
Anna <narnia.soup@gmail.com>
2023-11-13 16:28:52 +0200

    Main menui

---
+++ Summerhouse Project/Assets/Scripts/HighScores.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts
+{
+    [Serializable]
+    class HighScores
+    {
+        public HighScore[] scores;
+    }
+    [Serializable]
+    class HighScore
+    {
+        public int id = 0;
+        public string playerName = "";
+        public float score = 0.0f;
+        public string playtime = "";
+
+    }
+}

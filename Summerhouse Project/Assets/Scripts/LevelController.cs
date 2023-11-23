a9476497a2664bb36146100fa92520ed1fcc4f84
Anna <narnia.soup@gmail.com>
2023-11-23 19:23:15 +0200

    TimeScore added

--- Summerhouse Project/Assets/Scripts/LevelController.cs
+++ Summerhouse Project/Assets/Scripts/LevelController.cs
@@ -2,25 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using Assets.Scripts;
+using System;
 
 public class LevelController : MonoBehaviour
 {
     public TMP_InputField tmpIfTime;
     public TMP_InputField tmpIfPoints;
+    public TMP_InputField tmpIfTimeScore;
     public float initialTime;
     public float taskTime;
     public int timeScore; // ajasta tulevat pisteet
-    public float actionScore; //toiminnoista tulevat pisteet > näytetään pelaajalle valintoja tehdessä
-    public float playerScore; // pisteet yhteensä
+    public int actionScore; //toiminnoista tulevat pisteet > näytetään pelaajalle valintoja tehdessä
+    public int totalTimeScore;
+    public int totalActionScore;
+    public int playerScore; // pisteet yhteensä
+    public bool fireIsOut;
     private float timeLeft;
+    private float taskTimeLeft;
 
 
     // Start is called before the first frame update
 
     void Start()
     {
+        
+        Time.timeScale = 1;
         timeLeft = initialTime;
+        taskTimeLeft = taskTime;
+        fireIsOut = true;
+        actionScore = 0;
+        totalActionScore = 0;
+        playerScore = 0;
         StartCoroutine("updateLevel");
+
+
     }
     IEnumerator updateLevel()
     {
@@ -31,9 +47,17 @@ public class LevelController : MonoBehaviour
             if (timeLeft > 0)//jos aikaa on jäljellä vähennetään intervalli jäljellä olevasta ajasta
             {
                 timeLeft -= interval;
+                if (timeLeft == 180)
+                {
+                    fireIsOut = false;
+                    Debug.Log("Fire started");
+                    StartCoroutine("countTaskTime");
+                }
             }
             else //ei tee toistaiseksi mitään kun aika loppuu
-            { }
+            {
+                Time.timeScale = 0;
+            }
         }
     }
 
@@ -45,15 +69,63 @@ public class LevelController : MonoBehaviour
         tmpIfTime.text = string.Format("{0:00} : {1:00}", minutes, seconds); //muotoilee ajan minuuteiksi ja sekunneiksi
     }
 
-    int countTimeScore()
+    void DisplayTimeScore()
     {
-        taskTime = 60;
-        return timeScore;
+        Debug.Log("Display time score");
+        timeScore = (int) taskTimeLeft*10; 
+        tmpIfTimeScore.text = "+" + timeScore.ToString();
+    }
+
+    void DisplayPoints()
+    {
+        //Debug.Log("Display points");
+        playerScore = totalTimeScore + totalActionScore;
+        tmpIfPoints.text = playerScore.ToString();
+    }
+
+    IEnumerator countTaskTime()
+    {
+        Debug.Log("Task time count started...");
+        float interval = 1f;
+        taskTimeLeft = 60; // Initialize taskTimeLeft before entering the loop
+
+        for (; ; )
+        {
+            yield return new WaitForSeconds(interval);
+            if (Input.GetKeyDown(KeyCode.P))
+            {
+                Debug.Log("P key pressed");
+                fireIsOut = true;
+                Debug.Log("Fire is out");
+                Debug.Log("Task time left " +taskTimeLeft);
+                DisplayTimeScore();
+                totalTimeScore = totalTimeScore + timeScore;
+                break;
+            }
+
+            if (taskTimeLeft > 0)
+            {
+                taskTimeLeft -= interval;
+            }
+            else
+            {
+                Debug.Log("Task Time over");
+            }
+        }
+    }
+
+
+
+    public void putOutFire()
+    {
+        fireIsOut = false;
+        Debug.Log("fire out");
     }
 
     // Update is called once per frame
     void Update()
     {
         DisplayTime(timeLeft);
+        DisplayPoints();
     }
 }

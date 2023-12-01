acd109f02a4c9fcce1bcbdc3a623e8ab8181fa4d
Anna <narnia.soup@gmail.com>
2023-12-01 09:14:12 +0200

    Update LevelController.cs

--- Summerhouse Project/Assets/Scripts/LevelController.cs
+++ Summerhouse Project/Assets/Scripts/LevelController.cs
@@ -61,8 +61,8 @@ public class LevelController : MonoBehaviour
                 {
                     GlobalVariableStorage.level1 = false;
                     GlobalVariableStorage.level2 = true;
-                    blanket.tag = "level2";
-                    extinguisher.tag = "level2";
+                    //blanket.tag = "level2";
+                    //extinguisher.tag = "level2";
                     GlobalVariableStorage.fireIsOut = true;
                     Debug.Log("Level 2");
                     Debug.Log("Fire started");
@@ -192,11 +192,11 @@ public class LevelController : MonoBehaviour
 
 
 
-    public void putOutFire()
-    {
-        GlobalVariableStorage.fireIsOut = false;
-        Debug.Log("fire out");
-    }
+    //public void putOutFire()
+    //{
+    //    GlobalVariableStorage.fireIsOut = false;
+    //    Debug.Log("fire out");
+    //}
 
     // Update is called once per frame
     void Update()
